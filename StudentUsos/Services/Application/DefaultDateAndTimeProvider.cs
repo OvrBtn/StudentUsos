51fa6ba1682003b2c053cc4cf6b419edcc986d11
ovrbtn <ovrbtn@gmail.com>
2025-11-20 21:02:18 +0100

    Change base date

--- StudentUsos/Services/Application/DefaultDateAndTimeProvider.cs
+++ StudentUsos/Services/Application/DefaultDateAndTimeProvider.cs
@@ -4,5 +4,5 @@ namespace StudentUsos.Services.Application;
 public class DefaultDateAndTimeProvider : IDateAndTimeProvider
 {
     public DateTime Now => DateTimeOffset.Now.DateTime;
-    public DateTimeOffset UtcNow => DateTimeOffset.UtcNow.DateTime;
+    public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
 }
--- StudentUsos/Services/Application/GuestDateAndTimeProvider.cs
+++ StudentUsos/Services/Application/GuestDateAndTimeProvider.cs
@@ -3,6 +3,6 @@ namespace StudentUsos.Services.Application;
 
 public class GuestDateAndTimeProvider : IDateAndTimeProvider
 {
-    public DateTime Now => new(2025, 9, 11);
-    public DateTimeOffset UtcNow => new(new(2025, 9, 11));
+    public DateTime Now => new(2025, 6, 2);
+    public DateTimeOffset UtcNow => new(new(2025, 6, 2));
 }

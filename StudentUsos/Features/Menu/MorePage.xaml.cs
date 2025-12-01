402ccdb069f68b6c59adcc2c119c645dc22370a0
ovrbtn <ovrbtn@gmail.com>
2025-12-01 22:56:12 +0100

    Hide campus map button in menu for other installations

--- StudentUsos/Features/Menu/MorePage.xaml.cs
+++ StudentUsos/Features/Menu/MorePage.xaml.cs
@@ -1,12 +1,20 @@
 ﻿using StudentUsos.Controls;
+using StudentUsos.Features.Authorization.Services;
 
 namespace StudentUsos.Features.Menu;
 
 public partial class MorePage : CustomContentPageNotAnimated
 {
-    public MorePage(MoreViewModel moreViewModel)
+    public MorePage(MoreViewModel moreViewModel, IUsosInstallationsService usosInstallationsService)
     {
         BindingContext = moreViewModel;
         InitializeComponent();
+
+        const string campusMapAllowedInstallation = "https://usosapps.put.poznan.pl/";
+        var installation = usosInstallationsService.GetCurrentInstallation();
+        if (installation != campusMapAllowedInstallation)
+        {
+            campusMapButton.IsVisible = false;
+        }
     }
 }

4e3a339480f761dfe17c8a3739f94594135a3147
Olivier Betman <76904408+OvrBtn@users.noreply.github.com>
2025-12-06 18:45:12 +0100

    Fix wrong notifications about changes in schedule (#60)

--- StudentUsos/Platforms/Android/ActivitiesSynchronizationWorker.cs
+++ StudentUsos/Platforms/Android/ActivitiesSynchronizationWorker.cs
@@ -83,6 +83,20 @@ public class ActivitiesSynchronizationWorker : Worker
                 return false;
             }
 
+            //There is a strange bug which happens maybe once a week or two which causes this function 
+            //to inform about all activities getting removed even though they weren't removed.
+            //I wasn't able to reproduce but following the code I don't see anything that could cause this issue
+            //on the app or the server side. Also one user once reported having the schedule indicate that all next 7 days are days off
+            //even though they weren't (before this feature was implemented) so the only plausible explanation is that 
+            //there is some weird bug in USOS API which can cause it to sometimes return an empty list.
+            //This workaround will potentially skip informing about some real changes but it's better to skip some notifications
+            //than spamming fake ones.
+            if (local.AllActivities.Count > 0 && remote.AllActivities.Count == 0)
+            {
+                return false;
+            }
+
+
             activitiesRepository.Replace(remote.Result);
 
             bool areNotificationsEnabled = localStorageManager.GetBool(LocalStorageKeys.ActivitiesSynchronizationBackgroundWorker_ShouldSendNotifications, true);

7a31880bc638221db627793735430ca9d2e8b627
ovrbtn <ovrbtn@gmail.com>
2025-12-09 00:30:14 +0100

    Make property nullable

--- StudentUsos/Features/Grades/Services/CourseIdJsonObject.cs
+++ StudentUsos/Features/Grades/Services/CourseIdJsonObject.cs
@@ -10,7 +10,7 @@ internal partial class FinalGradeJsonContext : JsonSerializerContext
 internal class CourseIdJsonObject
 {
     [JsonPropertyName("course_units_grades")]
-    public Dictionary<string, List<Dictionary<string, FinalGrade>>> CourseUnitsGrades { get; set; }
+    public Dictionary<string, List<Dictionary<string, FinalGrade>>>? CourseUnitsGrades { get; set; }
     /// <summary>
     /// In case of USOS API for PUT this is always just an empty list.
     /// But in case of other universities which use one total grade for all class types this will contain 
